9d74325ae83245f6c2da6c442291db3ab1e43053
Julik Tarkhanov <me@julik.nl>
2024-02-03 11:20:09 +0100

    Apply explicit chunked encoding
    otherwise it falls on Puma/other Rack server to determine whether chunking has to be applied. While a good-natured webserver is supposed to chunk automatically, why not do it ourselves if we can?

--- lib/zipline.rb
+++ lib/zipline.rb
@@ -2,6 +2,7 @@ require 'content_disposition'
 require "zipline/version"
 require 'zip_tricks'
 require "zipline/zip_generator"
+require "zipline/chunked"
 
 # class MyController < ApplicationController
 #   include Zipline
@@ -18,7 +19,28 @@ module Zipline
     headers['Content-Type'] = Mime::Type.lookup_by_extension('zip').to_s
     response.sending_file = true
     response.cache_control[:public] ||= false
-    self.response_body = zip_generator
+
+    # Disables Rack::ETag if it is enabled (prevent buffering)
+    # see https://github.com/rack/rack/issues/1619#issuecomment-606315714
     self.response.headers['Last-Modified'] = Time.now.httpdate
+
+    if request.get_header("HTTP_VERSION") == "HTTP/1.0"
+      # If HTTP/1.0 is used it is not possible to stream, and if that happens it usually will be
+      # unclear why buffering is happening. Some info in the log is the least one can do.
+      logger.warn { "The downstream HTTP proxy/LB insists on HTTP/1.0 protocol, ZIP response will be buffered." } if logger
+      self.response_body = zip_generator
+    else
+      # Disable buffering for both nginx and Google Load Balancer, see
+      # https://cloud.google.com/appengine/docs/flexible/how-requests-are-handled?tab=python#x-accel-buffering
+      response.headers["X-Accel-Buffering"] = "no"
+
+      # Make sure Rack::ContentLength does not try to compute a content length,
+      # and remove the one already set
+      headers.delete("Content-Length")
+
+      # and send out in chunked encoding
+      headers["Transfer-Encoding"] = "chunked"
+      self.response_body = Zipline::Chunked.new(zip_generator)
+    end
   end
 end
